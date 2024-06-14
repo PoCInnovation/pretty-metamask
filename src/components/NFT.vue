e62d8054a9d47449feb42e48fd7ddf36a4783f0a
sacharbon <sacha.dujardin@epitech.eu>
2024-06-14 15:11:33 +0200

    feat(components/NFT.vue): retrieve ERC721 from a wallet

--- src/components/NFT.vue
+++ src/components/NFT.vue
@@ -1,65 +1,48 @@
 <script lang="ts">
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
+  import { getNFTsForOwner} from '@/getNFTsForOwner'
 
-export default defineComponent({
-  name: 'NFT',
-})
+  export default defineComponent({
+    name: 'NFT',
+  })
 
+  const NFTsList = await getNFTsForOwner("0x5dEccD005df66Cdd2F0b668610709e562eb3a00e", "eth-mainnet")
 </script>
 
 <script setup lang="ts">
   import { ref } from 'vue'
   import AddDialog from '@/components/AddDialog.vue'
   import AddBtn from '@/components/AddBtn.vue'
+  import NFTCard from '@/components/NFTCard.vue'
 
   const dialogVisible = ref(false)
-  const tokenURI = ref()
-
-  const handleDataRetrieved = (data: object) =>{
-    tokenURI.value = data
-    if (data)
-      dialogVisible.value = false
-  }
 </script>
 
 <template>
   <div id="container">
-    <div v-if="tokenURI" id="card">
-      <img :src="'https://ipfs.io/ipfs/' + tokenURI.image.replace('ipfs://', '')" alt="image">
-      <div>
-        <p id="title">{{ tokenURI.title }}</p>
-        <p id="desc">{{ tokenURI.description }}</p>
-      </div>
-    </div>
-    <AddBtn :label="'NFT'" @click="dialogVisible = true"/>
-    <AddDialog v-if="dialogVisible" :visible="dialogVisible" @close-dialog="dialogVisible = false" @data-retrieved="handleDataRetrieved"/>
+   <div v-for="NFT in NFTsList" :key="NFT.id">
+     <NFTCard v-if="NFT.tokenType === 'ERC721' && NFT.name" :name="NFT.name" :description="NFT.description" :content-type="NFT.image.contentType" :image="NFT.image.originalUrl"/>
+   </div>
   </div>
+  <AddBtn :label="'NFT'" @click="dialogVisible = true"/>
+  <AddDialog v-if="dialogVisible" :visible="dialogVisible" @close-dialog="dialogVisible = false" @data-retrieved="handleDataRetrieved"/>
 </template>
 
 <style scoped>
-  #card {
-    display:flex;
-    flex-direction: row;
-    align-items: center;
-  }
-  #title {
-    color: white;
-    font-size: 18px;
-  }
-  #desc {
-    font-size: 16px;
-    color: #A3A3A3;
-  }
-  img {
-    width: 80px;
-    height: 100%;
-    border-radius: 20px;
-    margin-right: 26px;
-  }
   #container {
     margin-top: 3.9vh;
     margin-bottom: 2.63vh;
     margin-inline: 15px;
     height: 27vh;
+    overflow: scroll;
+    position: relative;
+  }
+  #container::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
   }
 </style>
---
+++ src/components/NFTCard.vue
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'NFTCard',
+    props: {
+      name: String,
+      description: String,
+      contentType: String,
+      image: String
+    },
+    methods: {
+      truncate(str: string, n: number) {
+        return str.length > n ? str.slice(0, n - 1) + '...' : str;
+      },
+    }
+  });
+</script>
+
+<template>
+  <div id="card">
+    <img v-if="contentType && contentType.substring(0, 5) === 'image'" :src="image" alt="image">
+    <div v-else id="video">
+    </div>
+    <div>
+      <p id="title">{{ name }}</p>
+      <p id="desc" v-if="description">{{ truncate(description, 100) }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #video {
+    width: 80px;
+    height: 80px;
+    margin-right: 26px;
+    border-radius: 20px;
+    overflow: hidden;
+    background-color: #1E6BDE;
+  }
+  #card {
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    margin-bottom: 2vh;
+  }
+  #title {
+    color: white;
+    font-size: 18px;
+  }
+  #desc {
+    font-size: 16px;
+    color: #A3A3A3;
+  }
+  img {
+    width: 80px;
+    height: 100%;
+    border-radius: 20px;
+    margin-right: 26px;
+  }
+</style>
