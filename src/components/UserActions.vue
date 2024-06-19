c7563fe4f9cce9082b81250b6c9228f57b672109
sacharbon <sacha.dujardin@epitech.eu>
2024-06-19 17:15:33 +0200

    feat(UserActions.vue): front

--- src/components/UserActions.vue
+++ src/components/UserActions.vue
@@ -3,11 +3,29 @@
 
 <template>
   <div id="container">
+    <button>Buy</button>
+    <button id="sellBTN">Sell</button>
+    <button>Swap</button>
   </div>
 </template>
 
 <style scoped>
+  button {
+    width: 8.698vw;
+    height: 39px;
+    border-radius: 10px;
+    border: none;
+    background-color: #1E6BDE;
+    color: white;
+    font-size: 20px;
+  }
   #container {
     height: 7.8vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  #sellBTN {
+    margin-inline: 21px;
   }
 </style>
