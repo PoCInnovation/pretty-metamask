ee8c4d2a5d62cd81f1ee6fd333cfd546af2b4f0b
alexandre <alexandre.timal@epitech.eu>
2024-07-22 14:36:49 +0200

    [ADD] get all tokens from an adress

---
+++ src/api/alchemy.ts
@@ -0,0 +1,63 @@
+// alchemy.ts
+import type { AlchemyConfig } from "alchemy-sdk";
+import { Network, Alchemy } from "alchemy-sdk";
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const envPath = path.join(__dirname, '../../', '.env');
+dotenv.config({ path: envPath });
+
+interface Config {
+  apiKey: string;
+  network: Network;
+}
+
+const config: Config = {
+  apiKey: process.env.ALCHEMY_API_KEY || '',
+  network: Network.ETH_SEPOLIA,
+};
+
+const alchemy = new Alchemy(config as AlchemyConfig);
+
+export const getBalances = async (address: string) => {
+  try {
+    const balances = await alchemy.core.getTokenBalances(address);
+    const nonZeroBalances = balances.tokenBalances.filter((token) => {
+        return token.tokenBalance !== "0x0000000000000000000000000000000000000000000000000000000000000000";
+    });
+    console.log(`The balances of ${address} address are:`, nonZeroBalances);
+
+    let i = 1;
+
+    for (const token of nonZeroBalances) {
+      let balance: number = 0;
+      console.log(`token balance ${token.tokenBalance}`);
+      if (token.tokenBalance != null)
+        balance = parseInt(token.tokenBalance, 16);
+      const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+
+      balance = balance / Math.pow(10, metadata.decimals ?? 1);
+      const balanceStr: string = balance.toFixed(2);
+      console.log(`${i++}. ${metadata.name}: ${balanceStr} ${metadata.symbol} decimals is ${metadata.decimals}`);
+    }
+  } catch (error) {
+    console.error(`Failed to fetch token balances:`, error);
+  }
+};
+
+const runMain = async () => {
+  try {
+    await getBalances("0x439d71C47858c31bcaDc0EdBc41969d5BFB413f4");
+    process.exit(0);
+  } catch (error) {
+    console.error(`Error in main function:`, error);
+    process.exit(1);
+  }
+};
+
+if (require.main === module) {
+  runMain();
+}
