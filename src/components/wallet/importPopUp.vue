8e686df8a6539f149b9d0be91e90e7a3023e8b14
Grégor Sternat <gregor.sternat@kiln.fi>
2024-09-16 00:59:19 +0200

    feat: adding the base of the component for the import pop up

---
+++ src/components/wallet/importPopUp.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  name: "importPopUp",
+  mounted() {
+    this.$el.focus()
+  }
+}
+</script>
+
+<template>
+  <div
+    class="w-2/3 h-2/3 relative bg-background-gray rounded-lg flex flex-col z-10"
+    @keydown.esc="$emit('close')"
+    tabindex="0"
+  >
+  </div>
+</template>
