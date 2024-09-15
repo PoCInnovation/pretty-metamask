aa0e40f6d409dee5d591618abd1866adfdab1b69
Grégor Sternat <gregor.sternat@kiln.fi>
2024-09-16 01:04:53 +0200

    fix: remove the storage of the mnemonic

--- src/components/wallet/verticalNavbar.vue
+++ src/components/wallet/verticalNavbar.vue
@@ -1,16 +1,16 @@
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { mapGetters, mapActions } from 'vuex';
-import walletCard from '@/components/wallet/walletCard.vue';
-import mnemonicPopUp from '@/components/wallet/mnemonicPopUp.vue';
-import { generateWallet } from '@/utils/wallet';
-import type { WalletClient } from 'viem';
+import { defineComponent } from 'vue'
+import { mapGetters, mapActions } from 'vuex'
+import walletCard from '@/components/wallet/walletCard.vue'
+import mnemonicPopUp from '@/components/wallet/mnemonicPopUp.vue'
+import { generateWallet } from '@/utils/wallet'
+import type { WalletClient } from 'viem'
+import importPopUp from './importPopUp.vue'
 
 interface Account {
-  name: string;
-  address: string;
-  mnemonic: string;
-  wallet: WalletClient;
+  name: string
+  address: string
+  wallet: WalletClient
 }
 
 export default defineComponent({
@@ -18,41 +18,51 @@ export default defineComponent({
   components: {
     walletCard,
     mnemonicPopUp,
+    importPopUp
   },
   computed: {
-    ...mapGetters(['accounts', 'selectedAccount', 'accountReducedAddresses']),
+    ...mapGetters(['accounts', 'selectedAccount', 'accountReducedAddresses', 'password', 'walletCounter']),
     visibleMnemonicAccounts(): Account[] {
       return this.accounts.filter(
         (account: Account) => this.selectedAccount === account.address && this.isMnemonicVisible
-      );
+      )
     },
   },
   data() {
     return {
+      mnemonic: '',
       isMnemonicVisible: false,
-    };
+      isImportVisible: false
+    }
   },
   methods: {
     ...mapActions(['addAccount', 'selectAccount']),
     addAccountToStore() {
-      const { newWallet, mnemonic } = generateWallet();
+      const { newWallet, mnemonic } = generateWallet()
       const newAccount: Account = {
-        name: `Account ${this.accounts.length + 1}`,
+        name: `Account ${this.walletCounter + 1}`,
         address: newWallet.account.address,
-        mnemonic: mnemonic,
         wallet: newWallet
-      };
-      this.addAccount(newAccount);
-      this.isMnemonicVisible = true;
+      }
+      this.mnemonic = mnemonic
+      this.addAccount(newAccount)
+      this.isMnemonicVisible = true
     },
     selectAccountInStore(address: string) {
-      this.selectAccount(address);
+      this.selectAccount(address)
     },
-    closePopUp() {
-      this.isMnemonicVisible = false;
+    closeMnemonicPopUp() {
+      this.isMnemonicVisible = false
+      this.mnemonic = ''
     },
+    showImportPopUp() {
+      this.isImportVisible = true
+    },
+    closeImportPopUp() {
+      this.isImportVisible = false
+    }
   },
-});
+})
 </script>
 
 <template>
@@ -76,6 +86,12 @@ export default defineComponent({
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
           </svg>
         </div>
+        <div @click="showImportPopUp" class="bg-background-gray rounded-2xl p-3 flex items-center justify-center border-gray-600 border hover:bg-gray-600">
+          <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path d="M12 2a1 1 0 011 1v10.586l3.293-3.293a1 1 0 011.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L11 13.586V3a1 1 0 011-1z"/>
+            <path d="M5 18a1 1 0 011-1h12a1 1 0 110 2H6a1 1 0 01-1-1z"/>
+          </svg>
+        </div>
       </div>
     </div>
     <div class="flex items-center cursor-pointer p-4 w-full">
@@ -83,12 +99,17 @@ export default defineComponent({
       <span class="text-2xl">Settings</span>
     </div>
   </div>
+  <div v-if="isImportVisible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <importPopUp
+      @close="closeImportPopUp"
+    />
+  </div>
   <div v-if="isMnemonicVisible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
     <mnemonicPopUp
       v-for="account in visibleMnemonicAccounts"
       :key="account.address"
-      :mnemonic="account.mnemonic"
-      @close="closePopUp"
+      :mnemonic="mnemonic"
+      @close="closeMnemonicPopUp"
     />
   </div>
 </template>
